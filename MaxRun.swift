be88005486b7eb98aa965bd196fa75af9bd19e12
Ubuntu <ubuntu@ip-172-31-91-135.ec2.internal>
2021-12-10 18:47:54 +0000

    max string

---
+++ MaxRun.swift
@@ -0,0 +1,47 @@
+//
+//  MaxRun.swift
+//
+//  Created by Roman Cernetchi
+//  Created on 2021-12-10
+//  Version 1.0
+//  Copyright (c) 2021 Roman Cernetchi. All rights reserved.
+//
+//  This is a program that finds the largest run in the string.
+//
+import Foundation
+
+// This function calculates the max run and returns it.
+func maxRun(userInput: String) -> Int {
+
+    var count = 0
+    var maxCount = 0
+    var previous: Character = " "
+
+    // Counts how many consecutive occurrences
+    // for every character in userInput
+    for character in Array(userInput) {
+        if character == previous {
+            count += 1
+        } else {
+            count = 1
+            previous = character
+        }
+
+        if count > maxCount {
+            maxCount = count
+        }
+    }
+
+    return maxCount
+}
+
+// User Input
+print("Enter a string: ", terminator: "")
+let userInput = readLine()!
+
+// Calls maxRun() to get the max run
+let maxRunValue = maxRun(userInput: userInput)
+// Outputs it to the user
+print("Max Run: \(maxRunValue)")
+
+print("\nDone.")
